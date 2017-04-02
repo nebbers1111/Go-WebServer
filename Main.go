2872658854691cf73575fe193673f10de0e23694
Ben Coleman <benjamin.coleman@me.com>
2017-04-02 11:38:29 +0100

    Initial commit

---
+++ Main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	http.HandleFunc("/", indexFunc)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexFunc(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Hello Ben :) This is a message from a %s OS on a %s CPU", runtime.GOOS, runtime.GOARCH)
+}
